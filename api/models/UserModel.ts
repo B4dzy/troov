6de361206a39912d9956d6b768aa9ee01688a015
BenJ4u <benjamin@alpesian.com>
2023-02-08 07:05:46 +0100

    Mise en place de bcrypt et des méthodes UserSchema
    Permet de hasher le mot de passe d'un utilisateur et de comparer le mot de passe lors de l'authentification.

--- api/models/UserModel.ts
+++ api/models/UserModel.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from 'mongoose';
 import { IUser, IUserDocument, IUserModel } from '../interfaces/IUser';
+import { default as bcrypt } from 'bcryptjs';
 
 const UserSchema: Schema<IUserDocument> = new Schema(
   {
@@ -11,6 +12,24 @@ const UserSchema: Schema<IUserDocument> = new Schema(
 
 UserSchema.statics.buildUser = (args: IUser) => {
   return new User(args);
+}
+
+UserSchema.pre<IUserDocument>('save', async function (next) {
+  const user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+
+  next();
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword: string): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
 }
 
 const User = model<IUserDocument, IUserModel>("users", UserSchema);
