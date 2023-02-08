ea8641e121d1cc0a3f1893f8a1420a44165aa7d1
BenJ4u <benjamin@alpesian.com>
2023-02-08 06:23:47 +0100

    Mise en place de Mongoose
    Ajout du package Mongoose afin de mettre en place une connexion à MongoDB.

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,24 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://' + process.env.HOST_NAME + '/' + process.env.DATABASE_NAME);
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+connection.on('error', (error) => {
+  console.error(error);
+});
 
 app.use(express.json());
 
