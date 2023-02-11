f8beb36404e3fb346edf66b2f1be2c000a0401ba
BenJ4u <benjamin@alpesian.com>
2023-02-11 13:24:30 +0100

    [API] Suppression du mware auth sur certaines routes

--- api/routes/ItemRoutes.ts
+++ api/routes/ItemRoutes.ts
@@ -4,8 +4,8 @@ import { authenticated } from '../middlewares/authenticated';
 
 const ItemRoutes = express.Router();
 
-ItemRoutes.get('/item/:id', authenticated, ItemController.getItem);
-ItemRoutes.get('/item', authenticated, ItemController.getAllItem);
+ItemRoutes.get('/item/:id', ItemController.getItem);
+ItemRoutes.get('/item', ItemController.getAllItem);
 ItemRoutes.post('/item', authenticated, ItemController.createItem);
 ItemRoutes.put('/item/:id', authenticated, ItemController.updateItem);
 ItemRoutes.delete('/item/:id', authenticated, ItemController.deleteItem);
