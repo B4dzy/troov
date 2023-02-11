e4f804e9563e2a9630435387dc89e975a5a25acc
BenJ4u <benjamin@alpesian.com>
2023-02-11 07:02:36 +0100

    ✅ Register en place
    Il est désormais possible de s'inscrire.

--- pages/register.vue
+++ pages/register.vue
@@ -1,21 +1,21 @@
 <template>
     <section>
-        <form>
+        <form @submit.prevent="registerSubmit">
             <div>
                 <label for="email">
-                    <input type="text" name="email" placeholder="Email">
+                    <input type="text" v-model="email" name="email" placeholder="Email">
                 </label>
             </div>
 
             <div>
                 <label for="username">
-                    <input type="text" name="username" placeholder="Nom d'utilisateur">
+                    <input type="text" v-model="username" name="username" placeholder="Nom d'utilisateur">
                 </label>
             </div>
 
             <div>
                 <label for="password">
-                    <input type="password" name="password" placeholder="Mot de passe">
+                    <input type="password" v-model="password" name="password" placeholder="Mot de passe">
                 </label>
             </div>
 
@@ -27,3 +27,40 @@
         <NuxtLink to="/">Se connecter</NuxtLink>
     </section>
 </template>
+
+<script lang="ts">
+import { useAuthStore } from '~/store/auth';
+
+export default {
+    data() {
+        return {
+            email: '',
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        async registerSubmit() {
+            const config = useRuntimeConfig();
+            const auth = useAuthStore();
+            const router = useRouter();
+
+            await $fetch(`${config.public.API_BASE_URL}/user`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: this.email,
+                    username: this.username,
+                    password: this.password
+                })
+            })
+                .then(response => {
+                    auth
+                        .login({ email: this.email, password: this.password })
+                        .then((_response) => router.push("/dashboard"))
+                        .catch((error) => console.log("API error", error));
+                })
+                .catch(error => { throw error })
+        }
+    }
+}
+</script>
